f9fad7b5e7ba7b18e5b8e1217a9cfbef574e5f0e
Airiseru <dmdee2004@gmail.com>
2024-12-17 21:01:51 +0800

    init: initial creation of file

---
+++ mlfq.py
@@ -0,0 +1,30 @@
+"""
+PROJECT 1: MLFQ Scheduler in Python
+CS140 THR/FQR
+
+## Submitted By: ##
+Denise Mae B. Dee
+Nicholas John L. Reyes
+Jopeth Bryan B. Seda
+Kevin D. Trinidad
+
+## Project Specifications ##
+- Q1 (Top Priority Queue): RR with 4ms quantum
+- Q2: FCFS
+- Q3: SJF
+- no priority boosting
+
+Input:
+- number of processes (0 > x >= 11)
+- time allotment for Q1 (x >= 5)
+- time allotment for Q2 (x > 0)
+- time for context switch (0 >= x >= 5)
+- process name; arrival time; CPU Burst 1; I/O Burst 1; CPU Burst 2; etc.
+
+Output:
+- print events that occur every time stamp (arriving process, processing finishing its total burst time, state of the ready queue, process currently running in the CPU, processes currently runnign their IO, process demotion)
+- after all processes finish, print
+    - turn-around time for each process (arranged alphabetically)
+    - average turn-around time
+    - waiting time for each process (arranged alphabetically)
+"""
