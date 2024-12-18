b7fc140f46f29de30af10d252aee82097a406d17
Njlr41 <nicholasjohnreyes@gmail.com>
2024-12-18 09:47:59 +0800

    feet: made a dataclass for the Scheduler

--- mlfq.py
+++ mlfq.py
@@ -55,6 +55,39 @@ class Process:
     def print_waiting_time(self) -> str:
         return f"{self.get_waiting_time()} ms"
 
+@dataclass
+class Scheduler:
+    cpu: Process
+    process_list: list[Process]
+    arriving_list: list[Process]
+    queue_one: list[Process]
+    queue_two: list[Process]
+    queue_three: list[Process]
+    time: int
+        
+    """Function to get Arriving processes"""
+    def get_arriving_processes(self):
+        for process in self.process_list:
+            if process.arrival_time == self.time:
+                self.arriving_list.append(process) 
+        self.arriving_list = sorted(self.arriving_list, key = lambda p: p.name)
+
+    """Function to add Arriving processes to the Queue"""
+    def arriving_to_queue(self):
+        while self.arriving_list:
+            self.queue_one.append(self.arriving_list.pop(0))
+
+    """Function to move Queued process to CPU"""
+    def queue_to_CPU(self):
+        if self.queue_one and self.cpu.name == "":
+            self.cpu = self.queue_one.pop(0)
+
+        elif self.queue_two and self.cpu.name == "":
+            self.cpu = self.queue_two.pop(0)
+            
+        elif self.queue_three and self.cpu.name == "":
+            self.cpu = self.queue_three.pop(0)
+
 """Function to keep looping input until valid"""
 def input_int_loop(min_inp: int, max_inp: float) -> int:
     user_input = -5
@@ -73,18 +106,9 @@ def create_new_process(inp: str) -> Process:
                    [int(splitted_inp[i]) for i in range(2, len(splitted_inp)) if i%2==0], # even indices
                    [int(splitted_inp[i]) for i in range(2, len(splitted_inp)) if i%2!=0]) # odd indices
 
-"""Function to get Arriving processes"""
-def get_arriving_processes(process_list: list[Process], time: int) -> list[str]:
-    arriving_processes: list[str] = []
-    for process in process_list:
-        if process.arrival_time == time:
-            arriving_processes.append(process.name) 
-    return arriving_processes
-   
 if __name__ == "__main__":
     # "Global" Variables (temp)
-    process_list: list[Process] = []
-    time = 0
+    scheduler: Scheduler = Scheduler(Process("", -1, [-1], [-1], -1), [], [], [], [], [], 0)
 
     # Get scheduler details from input
     print("# Enter Scheduler Details #")
@@ -96,15 +120,42 @@ if __name__ == "__main__":
     # Get process details from input and put into a list
     print(f"# Enter {num_procs} Process Details #")
     for _ in range(num_procs):
-        process_list.append(create_new_process(input()))
+        scheduler.process_list.append(create_new_process(input()))
 
     print("# Scheduling Results #")
 
-    while (time < 10): #Temporary condition of time < 10.
-        print(f"At Time = {time}")
-        arriving_processes = get_arriving_processes(process_list, time)
-        if arriving_processes != []:
-            print(f"Arriving: {arriving_processes}") #Di ko knows how to print the array without the ''. #future worry
-        time = time + 1
+    while (scheduler.time < 10): #Temporary condition of time < 10.
+        print(f"At Time = {scheduler.time}")
+        """
+        1) Check for arriving processes
+        2) Move arriving processes to Queue One
+        3) Check if CPU should be cleared (process is done/ran out of quantum)
+        4) Move queued process to CPU if CPU is empty
+        5) Check for I/O
+        """
+        # Check for Arriving Processes
+        scheduler.get_arriving_processes()
+        # Move Arriving Processes to Queue One
+        scheduler.arriving_to_queue()
+        #Check if CPU should be cleared (process is done/ran out of quantum)
+
+        # Move Queued Process to CPU if CPU is Empty
+        scheduler.queue_to_CPU()
+        # Handle I/O
+
+        # Print Arriving
+        if scheduler.arriving_list != []:
+            print(f"Arriving : {[proc.name for proc in scheduler.arriving_list]}") #Di ko knows how to print the array without the ''. #future worry
+
+        # Print Queues
+        print(f"Queues : {[proc.name for proc in scheduler.queue_one]} ; {[proc.name for proc in scheduler.queue_two]} ; {[proc.name for proc in scheduler.queue_three]}")
+
+        # CPU
+        print(f"CPU : {scheduler.cpu.name}")
+        
+        # Print I/0
+
+        print()
+        scheduler.time = scheduler.time + 1
 
     print("SIMULATION DONE")
