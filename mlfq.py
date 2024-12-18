b7a9a4f3d3fd89bd609c9c1b1170a8483f526b67
Airiseru <dmdee2004@gmail.com>
2024-12-18 16:36:00 +0800

    fix: remove spaces between semicolons when printing the queue

--- mlfq.py
+++ mlfq.py
@@ -212,7 +212,7 @@ if __name__ == "__main__":
         if current_proc.idx > len(current_proc.cpu_burst) and current_proc.idx > len(current_proc.io_burst):
             # index > len(cpu_burst) essentially means that the process doesn't have to use the cpu anymore
             print(f"{scheduler.cpu.name} DONE") # print that process is done
-            
+
             current_proc.completion_time = scheduler.time
             scheduler.empty_cpu()
 
@@ -256,7 +256,7 @@ if __name__ == "__main__":
             scheduler.switch_time_pass += 1
 
         # Print Queues
-        print(f"Queues : {[proc.name for proc in scheduler.queue_one]} ; {[proc.name for proc in scheduler.queue_two]} ; {[proc.name for proc in scheduler.queue_three]}")
+        print(f"Queues : {[proc.name for proc in scheduler.queue_one]};{[proc.name for proc in scheduler.queue_two]};{[proc.name for proc in scheduler.queue_three]}")
 
         # CPU
         print(f"CPU : {scheduler.cpu.name}")
